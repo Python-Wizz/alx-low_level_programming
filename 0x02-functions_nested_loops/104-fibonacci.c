0fe661d9a9846f73e9d2a3a54d88f4234145187a
Python-Wizz <mr.taje@gmail.com>
2023-03-22 16:19:24 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,10 +14,10 @@ int main(void)
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
-	for (count = 0; count < 92; count ++)
+	for (count = 0; count < 92; count++)
 	{
 	sum = fib1 + fib2;
-	printf("%lu, ",sum);
+	printf("%lu, ", sum);
 	fib1 = fib2;
 	fib2 = sum;
 	}
