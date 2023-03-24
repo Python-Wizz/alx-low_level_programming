d576ad95bcee845d16b32ad195389a5f6e08a231
Python-Wizz <mr.taje@gmail.com>
2023-03-23 22:10:08 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigi - Check if a character is a digit
+ * _isdigit - Check if a character is a digit
  * @x: The number to be checked
  * Return: 1 for a character that will be a digit or 0 for any else
  */
@@ -9,9 +9,10 @@
 int _isdigi(int x)
 
 {
+
 	if (x >= 48 && <= 57)
 	{
 	return (1);
 	}
-	return
+	return (0);
 }
