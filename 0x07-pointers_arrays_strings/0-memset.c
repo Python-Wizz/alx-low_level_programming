3baf68bb224c285048c931afac495741ec0f1c7f
Python-Wizz <mr.taje@gmail.com>
2023-04-07 05:48:57 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * * _memset - fills a memory with a constant byte
+ * _memset - fills a memory with a constant byte
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
