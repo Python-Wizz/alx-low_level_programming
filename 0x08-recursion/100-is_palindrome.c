9a3aeb50332cc33c092afdac6550bc1f30a70154
Python-Wizz <mr.taje@gmail.com>
2023-04-09 11:50:17 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,7 +10,7 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 	else
-		return (1 + _strlen_recursion(s +1));
+		return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -24,7 +24,7 @@ int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
 	{
-		if (n1 == n2 || n1 == n2 +1)
+		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
 		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
@@ -32,7 +32,7 @@ int comparator(char *s, int n1, int n2)
 }
 
 /**
- * is_palindrome - detects if a string is palindrome.
+ * is_palindrome - detects if a string is a palindrome.
  * @s: string.
  * Return: 1 if s is a palindrome, 0 if not.
  */
