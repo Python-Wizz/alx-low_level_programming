315cd078c63901f932afa4e6b5d73c8a449dcce2
Python-Wizz <mr.taje@gmail.com>
2023-04-20 14:40:36 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- *  print_all - prints anything
- *  @format: list of types of arguments passed to the function
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
@@ -22,29 +22,29 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-						printf("%s%c", sep, va_arg(list, int));
-						break;
-					case 'i':
-						printf("%s%d", sep, va_arg(list, int));
-						break;
-					case 'f':
-						printf("%s%f", sep, va_arg(list, double));
-						break;
-					case 's':
-						str = va_arg(list, char *);
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
 					if (!str)
 						str = "(nil)";
-						printf("%s%s", sep, str);
-
-						break;
-					default:
-						i++;
-						continue;
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
 			}
 			sep = ", ";
-				i++;
+			i++;
 		}
 	}
+
 	printf("\n");
 	va_end(list);
 }
